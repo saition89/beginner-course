f6e709facfb9bd1d4f57c0644fcb008037d6ca23
saition89 <89878233+saition89@users.noreply.github.com>
2021-09-06 01:19:13 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,29 @@
+n = int(input()) * 2 + 1
+a = []
+b = [0] * n
+k = n ** 2 - 1
+st = 0
+for i in range(n):
+    c = b.copy()
+    a.append(c)
+for h in range(n * 2):
+    if h - st * 4 == 0:
+        for i in range(st, n - 1 - st):
+            a[i][n - 1 - st] = k
+            k -= 1
+    if h - st * 4 == 1:
+        for i in range(n - 1 - st, 0 + st, -1):
+            a[n - st - 1][i] = k
+            k -= 1
+    if h - st * 4 == 2:
+        for i in range(n - st - 1, 0 + st, -1):
+            a[i][0 + st] = k
+            k -= 1
+    if h - st * 4 == 3:
+        for i in range(st, n - 1 - st):
+            a[0 + st][i] = k
+            k -= 1
+        st += 1
+for i in range(n):
+    print(' '.join(list(map(str, a[i]))))
+    print()
